b6d0abaf8e65bb66e58f56d3b1701a0fccdbcc1c
selam lemma <oneiconforlife26@gmail.com>
2020-08-25 17:43:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,23 +7,23 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array.push("snow")
+  add_element_to_end_of_array.push([3],"snow")
 end
 
 def add_element_to_start_of_array(array, element)
-    add_element_to_start_of_array.unshift("caffin")
+    add_element_to_start_of_array.unshift([0]"caffin")
 end
 
 def remove_element_from_end_of_array(array)
-    remove_element_from_end_of_array.pop("snow")
+    remove_element_from_end_of_array.pop([3])
 end
 
 def remove_element_from_start_of_array(array)
-    remove_element_from_start_of_array.shift("caffin")
+    remove_element_from_start_of_array.shift([0])
 end
 
 def retrieve_element_from_index(array, index_number)
-    retrieve_element_from_index[1]
+    retrieve_element_from_index([1],[1])
 end
 
 def retrieve_first_element_from_array(array)
